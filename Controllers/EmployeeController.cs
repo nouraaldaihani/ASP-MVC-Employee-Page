778521fdaddc64395b8ffde40ccf6f4245f6ecb4
KFH <KFH@LAPTOP-TRN-136>
2024-04-16 14:29:46 +0300

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Employee> employees = new List<Employee>()
+            { 
+                new Employee { FirstName = "Noura", LastName = "Aldaihani", EmployeeId = 83333, Department = "IT", Email = "noura@gmail.com" }
+                };
+            return View(employees);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication3.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public string Department { get; set; }
+
+    }
+}
