ae5370d66ab5b17a1dd31885a40e24138b38e393
KFH <KFH@LAPTOP-TRN-136>
2024-04-17 13:35:27 +0300

    update

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,17 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 using WebApplication3.Models;
 
 namespace WebApplication3.Controllers
 {
     public class EmployeeController : Controller
     {
+
+        static List<Employee> employees = [
+
+        new Employee { FirstName = "Noura", LastName = "Aldaihani", EmployeeId = "83333", Department = "IT", Email = "noura@gmail.com" },
+            new Employee { FirstName = "Noura", LastName = "Aldaihani", EmployeeId = "83333", Department = "IT", Email = "noura@gmail.com" }
+         ];
+
         public IActionResult Index()
         {
-            List<Employee> employees = new List<Employee>()
-            { 
-                new Employee { FirstName = "Noura", LastName = "Aldaihani", EmployeeId = 83333, Department = "IT", Email = "noura@gmail.com" }
-                };
             return View(employees);
+        }
+
+        [HttpGet]
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Register(AddEmployeeForm form)
+        {
+            if (ModelState.IsValid) {
+
+                var EmployeeId = form.EmployeeId;
+                var FirstName = form.FirstName;
+                var LastName = form.LastName;
+                var Email = form.Email;
+                var Department = form.Department;
+                employees.Add(new Employee { EmployeeId = EmployeeId, FirstName = FirstName, LastName = LastName, Department = Department, Email = Email });
+
+                return RedirectToAction("Index");
+            }
+                return View(form);
         }
     }
 }
--- Models/AddEmployeeForm.cs
+++ Models/AddEmployeeForm.cs
@@ -1,13 +1,18 @@
-﻿namespace WebApplication3.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
 {
-    public class Employee
+    public class AddEmployeeForm
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
-
+        [Required]
         public string EmployeeId { get; set; }
-
+        [Required]
         public string Department { get; set; }
 
     }
